d59842f50c27fbb78d722d8d3498c846b3adebeb
William Silva <wsilva@Williams-MacBook-Pro.local>
2022-01-10 14:48:29 -0500

    add start script

--- server.js
+++ server.js
@@ -184,9 +184,10 @@ app.use((err,req,res,next) => {
 })
 
 
+const port = process.env.PORT || 3000;
 
 //Server listens for requests on port 3000
 //Print statement in terminal to indicate server is listening 
-app.listen(3000,() => {
-    console.log("Serving on port 3000.");
+app.listen(port,() => {
+    console.log(`Serving on port ${port}`);
 })
